cb2cde5dcba3be023eec3ac1c5694c577fe7e22d
androidchxroz <androidchxroz@gmail.com>
2015-04-06 16:31:40 +0800

    Manual Parser
    Manual classification parser to arff

--- PreprocessParser/src/ManualParser.java
+++ PreprocessParser/src/ManualParser.java
@@ -9,25 +9,25 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.File;
 
-public class TempParser {
-	
-	public static String relationName = "temp4000";
-	
-	public static String fileRead = "sample4000.txt";
-	public static String fileWritten = relationName + ".txt";
-	
+public class ManualParser {
+
+	public static String relationName = "training4000";
+
+	public static String fileRead = "manual4000.txt";
+	public static String fileWritten = relationName + ".arff";
+
 	public static String classA = "Politics";
 	public static String classB = "Economy";
 	public static String classC = "Social";
 	public static String classD = "Technology";
-	
+
 	public static String arffRelation = relationName;
 	public static String arffClass = "{" + classA + "," + classB + "," + classC + "," + classD + "}";
 	public static String arffID = "id";
 	public static String arffIDType = "NUMERIC";
 	public static String arffContent = "content";
 	public static String arffContentType = "STRING";
-	
+
 	public static String jsonID = "\"id\":\"";
 	public static String jsonContent = "\"content\":\"";
 	public static String jsonIDEnd = "\",";
@@ -38,45 +38,59 @@ public class TempParser {
 	public static String jsonRt = "Rt ";
 	public static String jsonHTTP = "http";
 	public static String jsonFormatEnd = "\"}]";
-	
+
 	public static void main(String[] args) {
-		 
-		TempParser obj = new TempParser();
+
+		ManualParser obj = new ManualParser();
 		obj.run();
-	 
-	  }
-	
+
+	}
+
 	public void run() {
-		 
+
 		BufferedReader br = null;
 		BufferedWriter bw = null;
 		String line = "";
-		
+
 		try {
-	 
+
 			File file = new File(fileWritten);
-			
+
 			// if file doesn't exists, then create it
 			if (!file.exists()) {
 				file.createNewFile();
 			}
-			
+
 			br = new BufferedReader(new FileReader(fileRead));
 			bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
-			
+
+			bw.write("@RELATION\t" + arffRelation + "\n\n");
+
+			bw.write("@ATTRIBUTE\tclass\t\t" + arffClass + "\n");
+			bw.write("@ATTRIBUTE\t" + arffID + "\t\t\t" + arffIDType + "\n");
+			bw.write("@ATTRIBUTE\t" + arffContent + "\t\t" + arffContentType + "\n\n");
+
+			bw.write("@DATA\n");
+
 			while ((line = br.readLine()) != null) {
-				
-				String[] arr = line.split(",");
-				
-				String tempStr = arr[2];
-				tempStr = tempStr.replace("'", "");
-				arr[2] = tempStr;
-				
-				bw.write(arr[0] + "," + arr[1] + ",\'" + arr[2].toLowerCase() + "\'\n");				
+
+				String[] splitStr = line.split(",");
+
+				String contentStr = splitStr[2];
+				// Preprocessing for punctuation
+				contentStr = contentStr.replace(jsonComma, "");
+				contentStr = contentStr.replace(jsonQuote, "");
+				// Preprocessing for whitespace
+				contentStr = contentStr.trim();
+				// Preprocessing for content toLowerCase()
+				contentStr = contentStr.toLowerCase();
+				splitStr[2] = contentStr;
+
+				bw.write(splitStr[0] + "," + splitStr[1] + ",\'" + splitStr[2] + "\'\n");				
 			}
-			
+
 			bw.close();
-	 
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -90,8 +104,8 @@ public class TempParser {
 				}
 			}
 		}
-	 
+
 		System.out.println("Done");
-	  }
+	}
 
 }
