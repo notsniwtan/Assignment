c4a652957195e9a0275fa19df4b762a221901f47
androidchxroz <androidchxroz@gmail.com>
2015-04-09 01:30:34 +0800

    Manual Classification to Arff Parser
    Renamed ManualParser

--- IRWeka/src/CorpusParser.java
+++ IRWeka/src/CorpusParser.java
@@ -12,19 +12,27 @@ import org.json.simple.parser.JSONParser;
 
 public class CorpusParser {
 
-	//public static String fileWritten = "fileWritten.txt";
-	//public static String fileWritten = "testSet.arff";
+	public static final String RELATION_NAME = "testing";
+	public static final String FILE_WRITTEN = RELATION_NAME + ".arff";
+	public static final String FILE_READ = "corpus.txt";
+	public static final String ATTRIBUTE_CLASS = "{Politics,Economy,Social,Technology}";
+	public static final String ATTRIBUTE_ID = "id";
+	public static final String ATTRIBUTE_ID_TYPE = "NUMERIC";
+	public static final String ATTRIBUTE_CONTENT = "content";
+	public static final String ATTRIBUTE_CONTENT_TYPE = "STRING";
 
 	public static void main(String[] args) throws IOException, java.text.ParseException, org.json.simple.parser.ParseException{
 
+		int count = 0;
+		
 		JSONParser parser = new JSONParser();     
 		Object obj;
-		System.out.println("Parsing JSON file");
+		System.out.println("[DEBUG] PARSING JSON FILE");
 
 		BufferedWriter bw = null;
 
 		try {
-			File file = new File(fileWritten);
+			File file = new File(FILE_WRITTEN);
 
 			// if file doesn't exists, then create it
 			if (!file.exists()) {
@@ -33,16 +41,15 @@ public class CorpusParser {
 
 			bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
 
-			obj = parser.parse(new FileReader("corpus.txt"));
+			obj = parser.parse(new FileReader(FILE_READ));
 			JSONObject jsonObject = (JSONObject) obj;
-			String name = (String) jsonObject.get("Name");
 			JSONArray documentList = (JSONArray) jsonObject.get("Documents");
 
 			//
-			bw.write("@RELATION\ttestSet\n");
-			bw.write("@ATTRIBUTE\tclass\t\t{Politics,Economy,Social,Technology}\n");
-			bw.write("@ATTRIBUTE\tid\t\t\tNUMERIC\n");
-			bw.write("@ATTRIBUTE\tcontent\t\tSTRING\n\n");
+			bw.write("@RELATION\t" + RELATION_NAME + "\n");
+			bw.write("@ATTRIBUTE\tclass\t\t" + ATTRIBUTE_CLASS + "\n");
+			bw.write("@ATTRIBUTE\t" + ATTRIBUTE_ID + "\t\t\t" + ATTRIBUTE_ID_TYPE + "\n");
+			bw.write("@ATTRIBUTE\t" + ATTRIBUTE_CONTENT + "\t\t" + ATTRIBUTE_CONTENT_TYPE + "\n\n");
 			bw.write("@DATA\n");
 			//
 
@@ -58,25 +65,28 @@ public class CorpusParser {
 				// (2) /r
 				// (3) comma
 				// (4) singleQuote
-				// (5) whitespace
-				// (6) toLowerCase()
+				// (5) toLowerCase()
+				// (6) http				
+				// (7) whitespace
 				String content = (String) innerObj.get("Content");
 				content = content.replace("\n", "");
 				content = content.replace("\r", "");
 				content = content.replace(",", "");
 				content = content.replace("'", "");
 				content = content.toLowerCase();
+				content = content.replaceAll("http.*", "");
 				content = content.trim();
 
 				System.out.println("id:" + id);
 				System.out.println("content:" + content);
 
-
-
 				bw.write(id + ",");
-				bw.write(content + "\n");
+				bw.write("'" + content + "'\n");
 
+				count++;
 			}
+			
+			System.out.println("\n[DEBUG] COUNT = " + count);
 
 			bw.close();
 
---
+++ IRWeka/src/ManualClassificationToArffParser.java
@@ -0,0 +1,108 @@
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
+
+public class ManualClassificationToArffParser {
+
+	public static final String RELATION_NAME = "testSet";
+
+	public static final String FILE_READ = "comparison1.txt";
+	public static final String FILE_WRITTEN = RELATION_NAME + ".arff";
+
+	public static final String CLASS_A = "Politics";
+	public static final String CLASS_B = "Economy";
+	public static final String CLASS_C = "Social";
+	public static final String CLASS_D = "Technology";
+
+	public static final String ARFF_CLASS = "{" + CLASS_A + "," + CLASS_B + "," + CLASS_C + "," + CLASS_D + "}";
+	public static final String ARFF_ID = "id";
+	public static final String ARFF_ID_TYPE = "NUMERIC";
+	public static final String ARFF_CONTENT = "content";
+	public static final String ARFF_CONTENT_TYPE = "STRING";
+
+	public static void main(String[] args) {
+
+		ManualClassificationToArffParser obj = new ManualClassificationToArffParser();
+		obj.run();
+
+	}
+
+	public void run() {
+
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		String line = "";
+
+		try {
+
+			File file = new File(FILE_WRITTEN);
+
+			// if file doesn't exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			br = new BufferedReader(new FileReader(FILE_READ));
+			bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+
+			bw.write("@RELATION\t" + RELATION_NAME + "\n\n");
+
+			bw.write("@ATTRIBUTE\tclass\t\t" + ARFF_CLASS + "\n");
+			bw.write("@ATTRIBUTE\t" + ARFF_ID + "\t\t\t" + ARFF_ID_TYPE + "\n");
+			bw.write("@ATTRIBUTE\t" + ARFF_CONTENT + "\t\t" + ARFF_CONTENT_TYPE + "\n\n");
+
+			bw.write("@DATA\n");
+
+			while ((line = br.readLine()) != null) {
+
+				String[] splitStr = line.split(",");
+
+				String content = splitStr[2];
+				
+				// Preprocess to remove
+				// (1) /n
+				// (2) /r
+				// (3) comma
+				// (4) singleQuote
+				// (5) toLowerCase()
+				// (6) http				
+				// (7) whitespace
+				content = content.replace("\n", "");
+				content = content.replace("\r", "");
+				content = content.replace(",", "");
+				content = content.replace("'", "");				
+				content = content.toLowerCase();
+				content = content.replaceAll("http.*", "");
+				content = content.trim();
+				
+				splitStr[2] = content;
+
+				bw.write(splitStr[0] + "," + splitStr[1] + ",\'" + splitStr[2] + "\'\n");				
+			}
+
+			bw.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		System.out.println("[DEBUG] END OF RUN");
+	}
+
+}
